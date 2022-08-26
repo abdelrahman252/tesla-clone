05710976b6cb17766a8ccd19b969502463b3994d
Abdelrahman Ahmed <73317105+abdelrahman252@users.noreply.github.com>
2022-08-26 05:43:45 +0100

    Add files via upload

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,92 +1,108 @@
-import React from 'react'
-import styled, { keyframes } from 'styled-components'
-import Fade from 'react-reveal/Fade';
-import "../index.css"
+import React from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import "../index.css";
 
-const Section = ({ title, description, leftTextBtn, rightTextBtn, backgroundImg }) => {
-    return (
-        <Wrap bgImage={backgroundImg} className="sec">
-            <Fade bottom>
-                <ItemText>
-                    <h1>{title}</h1>
-                    <p>{description}</p>
-                </ItemText>
-            </Fade>
-            <Buttons>
-                <Fade bottom>
-                    <ButtonGroup>
-                        <LeftButton>
-                            {leftTextBtn}
-                        </LeftButton>
-                        {rightTextBtn &&
-                            <RightButton>
-                                {rightTextBtn}
-                            </RightButton>
-                        }
-                    </ButtonGroup>
-                </Fade>
-                <img  src="/images/down-arrow.svg" alt=''/>
-            </Buttons>
-        </Wrap>
-    )
-}
+const Section = ({
+  title,
+  description,
+  leftTextBtn,
+  rightTextBtn,
+  backgroundImg,
+}) => {
+  return (
+    <>
+      <Wrap bgImage={backgroundImg} className="sec">
+        <motion.div
+          initial={{ opacity: 0, transform: "translateY(200px)" }}
+          whileInView={{
+            opacity: 1,
+            transform: "translateY(-1px)",
+          }}
+          transition={{ duration: 0.5 }}
+        >
+          <ItemText>
+            <h1>{title}</h1>
+            <p>{description}</p>
+          </ItemText>
+        </motion.div>
+        <Buttons>
+          <motion.div
+            initial={{ opacity: 0, transform: "translateY(200px)" }}
+            whileInView={{
+              opacity: 1,
 
-export default Section
+              transform: "translateY(-1px)",
+            }}
+            transition={{ duration: 0.5 }}
+          >
+            <ButtonGroup>
+              <LeftButton>{leftTextBtn}</LeftButton>
+              {rightTextBtn && <RightButton>{rightTextBtn}</RightButton>}
+            </ButtonGroup>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 1, transform: "translateY(5px)" }}
+            animate={{opacity: 1, transform: "translateY(0px)" }}
+            transition={{ duration: 0.2, yoyo: Infinity }}
+          >
+            <img src="/images/down-arrow.svg" alt="" />
+          </motion.div>
+        </Buttons>
+      </Wrap>
+    </>
+  );
+};
+
+export default Section;
 
 const Wrap = styled.div`
-    width: 100%;
-    height: 100vh;
-    background-position: center;
-    background-size: cover;
-    background-repeat: no-repeat;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-direction: column;
-    background-image: ${props => `url("/images/${props.bgImage}")`};
- 
-
-`
+  width: 100%;
+  height: 100vh;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
+`;
 const ItemText = styled.div`
-    padding-top: 15vh;
-    text-align: center;
-`
+  padding-top: 15vh;
+  text-align: center;
+`;
 const ButtonGroup = styled.div`
-    display: flex;
-    margin-bottom: 30px;
-    @media (max-width: 768px) {
-        flex-direction: column;
-    }
-    `
-
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
 
 const LeftButton = styled.div`
-    background-color: rgba(23,26,32,.8);
-    height: 40px;
-    width: 256px;
-    color: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 100px;
-    opacity: 0.8;
-    textTrans-form: uppercase;
-    fontSize: 12px;
-    cursor: pointer;
-    margin: 8px
-`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.8;
+  texttrans-form: uppercase;
+  fontsize: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
 const RightButton = styled(LeftButton)`
-    background-color: white;
-    opacity: 0.65;
-    color: black;
-    fontWeight: 500;
-`
+  background-color: white;
+  opacity: 0.65;
+  color: black;
+  fontweight: 500;
+`;
 
-
-
-const Buttons = styled.div`
-
-`
+const Buttons = styled.div``;
 
 // const updown = keyframes`
 //     0%,
